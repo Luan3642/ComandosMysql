7f07dd47800e21278911edcb9fd1e1a060a4fd06
Luan3642 <luan3642@hotmail.com>
2017-01-01 11:27:30 -0200

    COMANDO MYSQL

---
+++ src/comandomysql/ComandoMysql.java
@@ -0,0 +1,40 @@
+package comandomysql;
+
+public class ComandoMysql {
+
+    public static void main(String[] args) {
+        /* COMO REMOVER UMA COLUNA
+	 alter table  lote
+	 drop column cor;
+	 
+	 
+	  ESCOLHENDO UMA POSIÇÃO NA COLUNA, PARA SE ADICONAR
+	 alter table lote
+	 add column nome varchar(10) after garantia;
+	 
+	 
+	  COLOCANDO UMA POSIÇÃO NO PRIMEIRO LUGAR DA TABELA
+	  alter table lote
+	  add column qualquer int first;
+	  
+	  
+	  MODIFICANDO O TIPO DA ESTRUTURA
+	  alter table lote
+	  modify column entrada varchar(100);
+	  
+	  
+	   RENOMEANDO UMA COLUNA
+	  alter table lote
+      change column entrada entradinha varchar(20);
+	  
+	  
+	   RENOMEANDO O NOME DA TABELA
+	  alter table lote
+	  rename to JAILSON;
+	  
+	   APAGANDO A TABELA
+	  drop table lote;*/
+       }
+    
+    }
+
